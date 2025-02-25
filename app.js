deb051c325350788fdf5f4f5f5ed9c9a5b145d30
Jay-p04 <jayprajapati442005@user.noreply.github.com>
2025-02-25 12:33:48 +0530

    jayesh

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-function changeColor(color,delay ,nextcolor){
-    setTimeout(() => {
-        h1.style.color = color;
-        if(nextcolor){nextcolor();}
-    }, delay);
-}
-changeColor("red",1000,()=>{
-    changeColor("yello",2000);
-});
+let h1 = document.querySelector("h1");
+h1.style.color="red";
+// function changeColor(color, delay, nextcolor) {
+//     setTimeout(() => {
+//         h1.style.color = color;
+//         if (nextcolor) nextcolor();
+//     }, delay);
+// }
+
+// changeColor("red", 1000, () => {
+//     changeColor("yellow", 2000, () => {
+//         changeColor("blue", 1500, () => {
+//             changeColor("green", 1000);
+//         });
+//     });
+// });
