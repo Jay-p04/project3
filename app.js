c197766f01d133701f6e515e3b3017ff33c9042e
Jay-p04 <jayprajapati442005@user.noreply.github.com>
2025-02-25 12:17:38 +0530

    add index

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello");
+function changeColor(color,delay ,nextcolor){
+    setTimeout(() => {
+        h1.style.color = color;
+        if(nextcolor){nextcolor();}
+    }, delay);
+}
+changeColor("red",1000,()=>{
+    changeColor("yello",2000);
+});
